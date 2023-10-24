e1bfcda1e317e6b7d3fd1c63744a3ffb3f9d48da
NhatNK-43 <khanh.nhat.365@gmail.com>
2023-10-24 08:20:12 +0700

    Nhat submit exercise ss05

--- ss05_form_router/exercise2/src/App.js
+++ ss05_form_router/exercise2/src/App.js
@@ -2,17 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import {FormMedical} from "./components/FormMedical";
 import {Routes, Route} from "react-router-dom"
-import {NotFound} from "./components/NotFound";u
+import {NotFound} from "./components/NotFound";
 import {ToastContainer} from "react-toastify";
 function App() {
   return (
     <>
         <Routes>
-        {/*    <Route path"/" element={<FormMedical/>}></Route>*/}
-        {/*    <Route path"*" element={<NotFound/>}></Route>*/}
+            <Route path="/" element={<FormMedical/>}></Route>
+            <Route path="*" element={<NotFound/>}></Route>
         </Routes>
         <ToastContainer></ToastContainer>
-        <FormMedical/>
     </>
   );
 }
